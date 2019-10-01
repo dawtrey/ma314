10b51aaaff32500929f2f57e69e5dc254fab72b0
dawtrey <dawtreytom@gmail.com>
2019-10-01 20:34:32 +0100

    first

---
+++ peak.py
@@ -0,0 +1,8 @@
+l = [1,2,3,2,1]
+
+def peak(l):
+    m = cat(l,l[-1] - 1)
+    for i in len(l):
+        if l[i] > l[i+1]
+            return l[i]
+## iterate on middle n - 2 numbers and write special cases for first and last numbers. 
