162891fb3eaf86f27c36fea35b4315d3e1c1ea1f
dawtrey <dawtreytom@gmail.com>
2019-10-02 20:29:08 +0100

    peak iter done

--- peak.py
+++ peak.py
@@ -1,9 +1,14 @@
-l = [1,1,1,1,1,2,3,2,1]
+l1 = [1,1,1,1,1,2,3,2,1]
+l2 = [3, 2, 1]
+l3 = [1,2,3,4,5]
 
 def peak(x):
-    for i in range(len(x)):
-        if l[i] > l[i + 1]:
+    short = len(x) -1 
+    for i in range(short):
+        if x[i] > x[i + 1]:
             return i
+        else: return short 
 
-print(peak(l))
-
+print("The peak is at index: " + str(peak(l1)))
+print("The peak is at index: " + str(peak(l2)))
+print("The peak is at index: " + str(peak(l3)))
