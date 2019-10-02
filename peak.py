66df67f829ad1da76bd8e8ba8380d257f536de49
dawtrey <dawtreytom@gmail.com>
2019-10-02 19:42:28 +0100

    peak actually complete

--- peak.py
+++ peak.py
@@ -1,13 +1,9 @@
 l = [1,1,1,1,1,2,3,2,1]
 
 def peak(x):
-    a = x[0] - 1
-    z = x[-1] - 1
-    m = [a] + x + [z]  
-    for i in range(len(m)):
-        if m[i] > m[i + 1]:
+    for i in range(len(x)):
+        if l[i] > l[i + 1]:
             return i
-    
 
 print(peak(l))
 
