464fbee01c787ca2ccc9fe45de3de7281db34296
dawtrey <dawtreytom@gmail.com>
2019-10-07 08:15:35 +0100

    peak perfect

--- peak.py
+++ peak.py
@@ -1,8 +1,20 @@
-l = [1,2,3,2,1]
+l1 = [1,1,1,1,1,2,3,2,1]
+l2 = [3, 2, 1]
+l3 = [1,2,3,4,5]
 
-def peak(l):
-    m = cat(l,l[-1] - 1)
-    for i in len(l):
-        if l[i] > l[i+1]
-            return l[i]
-## iterate on middle n - 2 numbers and write special cases for first and last numbers. 
+def peak(x):
+    short = len(x) -1
+    shorter = len(x) - 2
+    if x[0] > x[1]:
+        return 0
+    else:
+        for i in range(shorter):
+        
+            if x[i+1] >= x[i + 2] and x[i] <= x[i + 1]
+                return i
+            else: return short
+   
+        
+print("The peak is at index: " + str(peak(l1)))
+print("The peak is at index: " + str(peak(l2)))
+print("The peak is at index: " + str(peak(l3)))
