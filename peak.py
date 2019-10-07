b6eaebf57749e422820f9ceca617f1494f8dbd6d
dawtrey <dawtreytom@gmail.com>
2019-10-07 11:14:45 +0100

    peak complete 1

--- peak.py
+++ peak.py
@@ -5,12 +5,13 @@ l3 = [1,2,3,4,5]
 def peak(x):
     short = len(x) -1
     shorter = len(x) - 2
-    if x[0] > x[1]:
+    reduced = x[1] - 0.5
+    if x[1] > reduced:
         return 0
     else:
         for i in range(shorter):
         
-            if x[i+1] >= x[i + 2] and x[i] <= x[i + 1]
+            if x[i+1] >= x[i + 2] and x[i] <= x[i + 1]:
                 return i
             else: return short
    
---
+++ peakrec.py
@@ -0,0 +1,20 @@
+l1 = [1,1,1,1,1,2,3,2,1]
+l2 = [3, 2, 1]
+l3 = [1,2,3,4,5]
+
+
+import math
+
+
+def rec(x):
+    hlen = math.floor(len(x)/2)
+    if len(1):
+
+    elif x[hlen-1] >= x[hlen]:
+          
+        x = x[0:hlen]
+
+    else: 
+        x = x[hlen:len(x)+1]
+
+print(len(l2))
